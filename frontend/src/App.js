b04ec832c226bb7c216124b718b21493fa16e01f
Draxer21 <dg6653756@gmail.com>
2025-09-21 20:03:20 -0300

    Eliminacion de modelos anteriores

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 // src/App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chatbot from "./Chatbot";
-import RutinaPage from "/pages/RutinaPages"; // ajusta la ruta si tu archivo se llama distinto
+import RutinaPage from ".//pages/RutinaPages"; // ajusta la ruta si tu archivo se llama distinto
 
 function App() {
   return (
     <BrowserRouter>
+    <header className="app-header">Fitter Chatbot </header>
       <Routes>
         <Route path="/" element={<Chatbot />} />
         <Route path="/rutina/:id" element={<RutinaPage />} />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,17 @@
-// src/App.js
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Chatbot from "./Chatbot";
-import RutinaPage from "/pages/RutinaPages"; // asegúrate que el archivo se llame igual
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Ruta principal: el chatbot */}
-        <Route path="/" element={<Chatbot />} />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-        {/* Ruta para abrir rutina en nueva pestaña */}
-        <Route path="/rutina/:id" element={<RutinaPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
